248191b5b5470f47c98969dc66eab39842966f34
asfakulkabir <106924755+asfakulkabir@users.noreply.github.com>
2024-02-16 17:18:21 +0600

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,40 @@
+// category menu js 
+let index = 1;
+
+const on = (listener, query, fn) => {
+	document.querySelectorAll(query).forEach(item => {
+		item.addEventListener(listener, el => {
+			fn(el);
+		})
+	})
+}
+
+on('click', '.selectBtn', item => {
+	const next = item.target.nextElementSibling;
+	next.classList.toggle('toggle');
+	next.style.zIndex = index++;
+});
+
+
+// slider js 
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 1,
+	spaceBetween:10,
+	pagination: {
+	  clickable: true,
+	},
+	breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      },
+  });
